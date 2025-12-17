c01b00977ebdfe7a73ffe5f574ce3d641cd10b94
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-12-17 19:39:02 +0530

    added udp and tcp servers

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,8 +15,31 @@ func main() {
 	})
 
 	fmt.Println("Server started on port 8080")
-	err := http.ListenAndServe(":8080", r)
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 8080})
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			n, _, err := conn.ReadFromUDP(buf)
+			if err != nil {
+				fmt.Println("Error reading from UDP:", err)
+				continue
+			}
+			fmt.Printf("Received %d bytes: %s\n", n, buf[:n])
+		}
+	}()
+
+	// we run tcp server, as well because udp is non-blocking and it will kill the server as soon as it started
+	go func() {
+		err := http.ListenAndServe(":8080", r)
+		if err != nil {
+			fmt.Println("Error starting server:", err)
+		}
+	}()
+
+	// wait for all goroutines to finish
+	select {}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/MdSadiqMd/AV-Chaos-Monkey
 
 go 1.24.10
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
