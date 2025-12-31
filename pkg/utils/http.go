a51cd652b3e710d8d531373d51ac5ea127ef2ce5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-01-01 00:49:41 +0530

    http helpers for scripts

---
+++ pkg/utils/http.go
@@ -0,0 +1,80 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type HTTPClient struct {
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
+func NewHTTPClient(baseURL string) *HTTPClient {
+	return &HTTPClient{
+		BaseURL: baseURL,
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func (c *HTTPClient) Get(path string) ([]byte, error) {
+	url := c.BaseURL + path
+	resp, err := c.HTTPClient.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("GET %s: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("GET %s: status %d", url, resp.StatusCode)
+	}
+
+	return io.ReadAll(resp.Body)
+}
+
+func (c *HTTPClient) Post(path string, body any) ([]byte, error) {
+	url := c.BaseURL + path
+
+	var bodyBytes []byte
+	var err error
+	if body != nil {
+		bodyBytes, err = json.Marshal(body)
+		if err != nil {
+			return nil, fmt.Errorf("marshal body: %w", err)
+		}
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		return nil, fmt.Errorf("create request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("POST %s: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read response: %w", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("POST %s: status %d: %s", url, resp.StatusCode, string(respBody))
+	}
+
+	return respBody, nil
+}
+
+func (c *HTTPClient) HealthCheck() error {
+	_, err := c.Get("/healthz")
+	return err
+}
